6b5976513d69b103994c20cbd1e828fcda1a9953
Shirodevolop <emilioelva11@gmail.com>
2022-05-26 18:36:58 +0700

    nav_drawer

---
+++ app/src/test/java/com/android/pertemuan_5_bagian_6_membuat_navigation_drawer/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.android.pertemuan_5_bagian_6_membuat_navigation_drawer
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
